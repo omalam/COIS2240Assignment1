24932c7e2a209f4e4e7e6091fe2a6c25fe7cc30f
Jeremy McConnell <jeremy.fmcc@gmail.com>
2017-01-27 11:31:48 -0500

    removed default auto generated comments

--- src/com/xuzrus/shapes/GeometricObject.java
+++ src/com/xuzrus/shapes/GeometricObject.java
@@ -1,7 +1,4 @@
 package com.xuzrus.shapes;
 
-/**
- * Created by Jeremy on 2017-01-27.
- */
 public class GeometricObject {
 }
--- src/com/xuzrus/shapes/Triangle.java
+++ src/com/xuzrus/shapes/Triangle.java
@@ -1,9 +1,6 @@
 package com.xuzrus.shapes;
 
-/**
- * Created by Jeremy on 2017-01-26.
- */
-public class Triangle {
+public class Triangle extends  GeometricObject {
 
     public double side1 = 1.0;
     public double side2 = 1.0;
