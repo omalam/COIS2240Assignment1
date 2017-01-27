31f2b48e2c7f69b7360316f5899654b66e3d1b8e
Jeremy McConnell <jeremy.fmcc@gmail.com>
2017-01-27 11:29:28 -0500

    added triangle class and initiali properties

---
+++ src/com/xuzrus/shapes/Triangle.java
@@ -0,0 +1,12 @@
+package com.xuzrus.shapes;
+
+/**
+ * Created by Jeremy on 2017-01-26.
+ */
+public class Triangle {
+
+    public double side1 = 1.0;
+    public double side2 = 1.0;
+    public double side3 = 1.0;
+
+}
