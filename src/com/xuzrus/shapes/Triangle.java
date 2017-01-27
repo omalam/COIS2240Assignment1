b772049b71e1c12256dd5988221a07fbb76bd1b5
Jeremy McConnell <jeremy.fmcc@gmail.com>
2017-01-27 11:38:45 -0500

    added overloaded Triangle constructors

--- src/com/xuzrus/shapes/Triangle.java
+++ src/com/xuzrus/shapes/Triangle.java
@@ -2,8 +2,32 @@ package com.xuzrus.shapes;
 
 public class Triangle extends  GeometricObject {
 
-    public double side1 = 1.0;
-    public double side2 = 1.0;
-    public double side3 = 1.0;
+    private double side1 = 0;
+    private double side2 = 0;
+    private double side3 = 0;
+
+    private final double default_side_length = 1.0;
+
+    public Triangle(double side1) {
+
+        this.side1 = side1;
+        this.side2 = this.default_side_length;
+        this.side3 = this.default_side_length;
+    }
+
+    public Triangle(double side1, double side2) {
+
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = this.default_side_length;
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+
+    }
 
 }
