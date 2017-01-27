cdbd10aff942ee5155fd8bbfc3eb369316a4207f
Jeremy McConnell <jeremy.fmcc@gmail.com>
2017-01-27 15:52:07 -0500

    fixed wrong case of print method

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,13 +1,12 @@
+import com.xuzrus.shapes;
+
 /**
  * Class implements testing of Triangle object as
  * specified in the Assignment 1 instruction pdf
  *
  * @author Jeremy McConnell
- * @see com.xuzrus.shapes.Triangle
+ * @see com.xuzrus.shapes
  */
-
-import com.xuzrus.shapes;
-
 public class Assignment1 {
 
     public static void main(String[] args) {
@@ -15,10 +14,10 @@ public class Assignment1 {
         Triangle triangle = new Triangle(1, 1.5, 1);
         triangle.setColor("yellow");
         triangle.setFilled(true);
-        System.out.println(triangle);
-        System.out.println("The area is " + triangle.getArea());
-        System.out.println("The perimeter is "
+        System.out.printIn(triangle);
+        System.out.printIn("The area is " + triangle.getArea());
+        System.out.printIn("The perimeter is "
                 + triangle.getPerimeter());
-        System.out.println(triangle);
+        System.out.printIn(triangle);
     }
 }
